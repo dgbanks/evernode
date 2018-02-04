4b46300b7f81362d587f44d8028f43d4431c6388
David <davidgbanks91@gmail.com>
2018-02-03 21:29:43 -0800

    Configuration,render welcome message

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
+  helper_method :logged_in?
+  
   def current_user
     @current_user ||= User.find_by_session_token(session[:session_token])
   end
@@ -21,7 +23,7 @@ class ApplicationController < ActionController::Base
   end
   
   def ensure_logged_in
-    
+    #
   end
   
 end
