33b77db988fe23c6b7874eece6a7bcd3c6ea18d3
David <davidgbanks91@gmail.com>
2018-02-27 14:04:57 -0800

    Link reducer

--- frontend/reducers/link_reducer.js
+++ frontend/reducers/link_reducer.js
@@ -1,5 +1,21 @@
 import {
     RECEIVE_LINK,
-    RECEIVE_CANVAS_LINKS,
+    RECEIVE_NODE_LINKS,
     REMOVE_LINK
 } from '../actions/link_actions';
+
+export const LinksReducer = (prevState = {}, action) => {
+  Object.freeze(prevState);
+  switch (action.type) {
+    case RECEIVE_LINK:
+      return Object.assign({}, prevState, { [action.link.data.id]: action.link.data });
+    case RECEIVE_NODE_LINKS:
+      return action.links.data;
+    case REMOVE_LINK:
+      let newState = Object.assign({}, prevState);
+      delete newState[action.link.data.id];
+      return newState;
+    default:
+      return prevState;
+  }
+};
